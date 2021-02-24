b93778b33eabb0f695ad28579f9105d4e40167f3
Andrei Vasile <andreibvasile@gmail.com>
2021-02-24 21:33:39 +0000

    added mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from 'dotenv'
-import {resolve} from 'path'
+import { resolve } from 'path'
 import server from './server'
+import mongoose from 'mongoose'
+
 
 dotenv.config({ path: resolve(__dirname, "../.env") })
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("Connection Successful")
+})
 
 server.listen(process.env.PORT, () => {
     console.log("Listening on " + process.env.PORT)
