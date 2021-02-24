f034eab62fb702f83607087cd7d21e6d5086c5c3
Andrei Vasile <andreibvasile@gmail.com>
2021-02-24 21:52:16 +0000

    updated the user model and implemented basic user registration

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,10 @@
-import {Schema, model} from 'mongoose'
+import { Schema, model } from 'mongoose'
 
 const userSchema = Schema({
-    email: {type: String, unique: true},
-    displayName: String,
-    password: String
+    username: { type: String, unique: true, require: true },
+    displayName: { type: String, require: true },
+    password: { type: String, require: true },
+    email: String // don't want to force the user to have a password, or at least not yet
 })
 
 export default model('User', userSchema)
