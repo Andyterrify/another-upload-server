48660290bc5e28e34ab6deabcca23f17f441c4c3
Andrei Vasile <andreibvasile@gmail.com>
2021-02-25 23:27:10 +0000

    added more codes for consistency

--- src/utils/clientErrors.js
+++ src/utils/clientErrors.js
@@ -1,13 +1,13 @@
-import errs from "./codes"
+import codes from './codes';
 
 export default {
-    missingField: async (res, field) => {
-        return res.status(errs.BAD_REQUEST).json({
-            error: `Missing field '${field}'`
-        })
-    },
-    duplicateField: async (res, err) => {
-        const field = Object.keys(err.keyValue)
-        return res.status(errs.CONFLICT).send(`An account with that field '${field}' already exists.`)
-    }
-}
+  missingField: async (res, field) => res.status(codes.BadRequest).json({
+    error: `Missing field '${field}'`,
+  }),
+  duplicateField: async (res, err) => {
+    const field = Object.keys(err.keyValue);
+    return res
+      .status(codes.Conflict)
+      .json({ error: `An account with field '${field}' already exists.` });
+  },
+};
